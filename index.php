a6d5173e1c920c616c9d07767b17c3dd0cca801e
jacoposergio <djago32@gmail.com>
2022-07-28 14:49:45 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+// Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche.
+// L'e-commerce vende prodotti per gli animali.
+// I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+// L'utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
+// BONUS:
+// Il pagamento avviene con la carta prepagata che deve contenere un saldo sufficiente all'acquisto.
+?>
